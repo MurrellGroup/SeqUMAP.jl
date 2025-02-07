3685884df0de23ddc961de180a3ce26bd628231c
Anton Oresten <antonoresten@gmail.com>
2025-02-07 15:22:18 +0100

    Add laziest test ever

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,5 +2,8 @@ using SeqUMAP
 using Test
 
 @testset "SeqUMAP.jl" begin
-    # Write your tests here.
+    seqs = [join(rand(('A','C','T','G'), 2000)) for _ in 1:100]
+    seqs_clean = clean_nt.(seqs, to_strip=['-'])
+    proj = sequmap(seqs_clean, 2)
+    @test size(proj) == (2, 100)
 end
